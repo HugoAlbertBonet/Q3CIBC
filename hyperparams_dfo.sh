b4b8f5f2aea59f2b663bb425bcf51c83d4b023ee
Hugo Albert Bonet <halbertb@discovery1.hpc.usc.edu>
2026-04-14 10:02:31 -0700

    trials 37-40 q3c ibc 8 dim

--- hyperparams_dfo.sh
+++ hyperparams_dfo.sh
@@ -4,9 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1 
+#SBATCH --gres=gpu:v100:1
 #SBATCH --mem=16G
-#SBATCH --time=6:00:00
+#SBATCH --time=24:00:00
 
 export WANDB_API_KEY=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
-srun uv run python hyperparam_search_dfo.py --run --params '{"SOFTMAX_TEMPERATURE": 0.5, "NUM_COUNTER_EXAMPLES": 32, "TRAINING_STEPS": 100000, "LANGEVIN_TRAIN_ITERATIONS": 100, "LR_DECAY_RATE": 0.995}'
+set -e
+srun uv run python hyperparam_search.py combinedv2_cpascounter_training.py --run --params '{"control_points": 30, "top_k_control_points": 30, "counter_examples": 8, "mse_weight": 20.0, "separation_weight": 0.1, "separation_loss": "entropy", "entropy_bandwidth": 0.1, "generator_infonce_weight": 0.05, "info_nce_weight": 1.0, "infonce_logit_clamp": 20.0, "learning_rate": 0.001, "training_steps": 200000}'
+srun uv run python hyperparam_search.py combinedv2_cpascounter_training.py --run --params '{"control_points": 30, "top_k_control_points": 30, "counter_examples": 16, "mse_weight": 20.0, "separation_weight": 0.1, "separation_loss": "entropy", "entropy_bandwidth": 0.1, "generator_infonce_weight": 0.05, "info_nce_weight": 1.0, "infonce_logit_clamp": 20.0, "learning_rate": 0.001, "training_steps": 200000, "cosine_t_max": 400000}'
+srun uv run python hyperparam_search.py combinedv2_cpascounter_training.py --run --params '{"control_points": 30, "top_k_control_points": 30, "counter_examples": 8, "mse_weight": 20.0, "separation_weight": 0.1, "separation_loss": "entropy", "entropy_bandwidth": 0.1, "generator_infonce_weight": 0.05, "info_nce_weight": 1.0, "infonce_logit_clamp": 20.0, "learning_rate": 0.001, "training_steps": 200000, "cosine_t_max": 400000}'
