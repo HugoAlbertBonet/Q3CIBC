1228f025087c851025015bad9b88e9117607753d
HugoAlbertBonet <hugo@DESKTOP-2EEF2QL.localdomain>
2026-07-14 19:27:09 -0700

    solved mkdir bug in pusht

--- scripts/submit_pusht_real.sh
+++ scripts/submit_pusht_real.sh
@@ -9,6 +9,7 @@ LOG_DIR=${PUSHT_LOG_DIR:-"${ROOT_DIR}/slurm_jobs/pusht"}
 mkdir -p "${LOG_DIR}"
 
 sbatch \
+    --chdir="${ROOT_DIR}" \
     --output="${LOG_DIR}/slurm_%A_%a.out" \
     --error="${LOG_DIR}/slurm_%A_%a.err" \
     scripts/train_pusht_real_array.sbatch
